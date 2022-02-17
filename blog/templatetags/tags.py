17fb61f7ce32efa29cf46096489fd726111fa887
Str1kez <vazinov@yandex.ru>
2022-02-18 00:35:45 +0300

    added top articles in sidebar, footer, top

--- blog/templatetags/tags.py
+++ blog/templatetags/tags.py
@@ -1,16 +1,21 @@
 from django import template
 
-from blog.models import Tag
+from blog.models import Tag, Article
 import datetime as dt
 
 register = template.Library()
 
 
 @register.simple_tag
-def get_tags(count: int=20):
+def article_tags(count: int = 20):
     return Tag.objects.get_queryset()[:count]
 
 
-@register.simple_tag()
-def get_date():
+@register.simple_tag
+def header_date():
     return dt.date.today()
+
+
+@register.simple_tag
+def popular_posts(count: int = 5):
+    return Article.objects.order_by('-views')[:count]
